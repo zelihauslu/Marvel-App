8c133aafe6086cea8062e48ed57e472688632cd3
zelihauslu <zelusluz@gmail.com>
2022-08-01 20:42:48 +0300

    font extension, modifier ; color extension created

---
+++ Marvel-App/Marvel-App/Extensions/Color+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Color+Extension.swift
+//  Marvel-App
+//
+//  Created by Zeliha Uslu on 1.08.2022.
+//
+
+import SwiftUI
+
+struct Color_Extension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Color_Extension_Previews: PreviewProvider {
+    static var previews: some View {
+        Color_Extension()
+    }
+}
