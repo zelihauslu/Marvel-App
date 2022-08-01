58b96e22f4804468cb5d5b75cf726498c94ed2e6
zelihauslu <zelusluz@gmail.com>
2022-08-01 18:10:30 +0300

    first commit

---
+++ Marvel-App/Marvel-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marvel-App
+//
+//  Created by Zeliha Uslu on 1.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Marvel-App/Marvel-App/Marvel_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marvel_AppApp.swift
+//  Marvel-App
+//
+//  Created by Zeliha Uslu on 1.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Marvel_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
