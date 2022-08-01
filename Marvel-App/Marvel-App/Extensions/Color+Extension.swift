969f9ab4e118c90dda1f48cc68cd926a7b1fee86
zelihauslu <zelusluz@gmail.com>
2022-08-01 20:47:58 +0300

    custom color extension

--- Marvel-App/Marvel-App/Extensions/Color+Extension.swift
+++ Marvel-App/Marvel-App/Extensions/Color+Extension.swift
@@ -5,16 +5,18 @@
 //  Created by Zeliha Uslu on 1.08.2022.
 //
 
+import Foundation
 import SwiftUI
 
-struct Color_Extension: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+extension Color {
 
-struct Color_Extension_Previews: PreviewProvider {
-    static var previews: some View {
-        Color_Extension()
+    static let accentColor = Color("accentColor")
+    static let marvelGrey = Color("marvelGrey")
+    static var marvelBlack: Color {
+        Color(UIColor { $0.userInterfaceStyle == .light ? UIColor(red: 47, green: 47, blue: 47, alpha: 1) : UIColor(red: 248, green: 245, blue: 245, alpha: 1) })
     }
+    static var marvelLight: Color {
+        Color(UIColor { $0.userInterfaceStyle == .light ? UIColor(red: 248, green: 245, blue: 245, alpha: 1) : UIColor(red: 47, green: 47, blue: 47, alpha: 1) })
+    }
+
 }
